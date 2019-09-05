51a7c3120e84a391cd4b727986128839e8e77866
ChenWei <1458010118@qq.com>
2019-09-05 14:29:39 +0800

    完善类型信息

--- amap/index.d.ts
+++ amap/index.d.ts
@@ -1,35 +1,105 @@
 import {IonicNativePlugin} from '@ionic-native/core';
 
 export declare class AMapPlugin extends IonicNativePlugin {
-    /**
-     * This function does something
-     * @param arg1 {string} Some param to configure something
-     * @param arg2 {number} Another param to configure something
-     * @return {Promise<any>} Returns a promise that resolves when something happens
-     */
-    functionName(arg1: string, arg2: number): Promise<any>;
-
     getLocation(): Promise<LocationResult | void>;
 
-    getWeatherInfo(params: { addressCode: string }): Promise<WeatherInfo | void>;
+    getWeatherInfo(params: { adCode: string }): Promise<WeatherInfo | void>;
 
     calculateDistance(params: { startLatitude: number, startLongitude: number, endLatitude: number, endLongitude: number }): Promise<number | void>;
-
-    geoFenceIn(params: { latitude: number, longitude: number, radius: number }): Promise<GeoFenceResult | void>;
-
-    geoFenceOut(params: { latitude: number, longitude: number, radius: number }): Promise<GeoFenceResult | void>;
-
-    geoFenceStayed(params: { latitude: number, longitude: number, radius: number }): Promise<GeoFenceResult | void>;
 }
 
 export interface LocationResult {
-
+    /**
+     * 定位精度
+     */
+    accuracy: number;
+    /**
+     * 区域编码
+     */
+    adCode: string;
+    /**
+     * 地址
+     */
+    address: string;
+    /**
+     * 城市|区
+     */
+    city: string;
+    /**
+     * 城市编码
+     */
+    cityCode: string;
+    /**
+     *精度
+     */
+    latitude: number;
+    /**
+     *纬度
+     */
+    longitude: number;
+    /**
+     * 当前定位点的AOI信息
+     */
+    aoiName: string;
+    /**
+     * 国家
+     */
+    country: string;
+    /**
+     * 城区信息
+     */
+    district: string;
+    /**
+     * 当前定位点的POI信息
+     */
+    poiName: string;
+    /**
+     * 省份
+     */
+    province: string;
+    /**
+     * 街道
+     */
+    street: string;
+    /**
+     * 街道号
+     */
+    streetNum: string;
+    /**
+     * 定位时间
+     */
+    locationTime: Date
 }
 
 export interface WeatherInfo {
-
-}
-
-export interface GeoFenceResult {
-
+    type: "live" | "forecast";
+    /**
+     * 天气
+     */
+    weather: string;
+    /**
+     * 温度
+     */
+    temperature: string;
+    /**
+     * 城市|区
+     */
+    city: string;
+    /**
+     * 省份
+     *
+     */
+    province: string;
+    /**
+     * 风向
+     */
+    windDirection: string;
+    /**
+     * 风力
+     */
+    windPower: string;
+    /**
+     * 湿度
+     */
+    humidity: string;
 }
